b0ff64f7cfe44f55cf4011405c3b898794bb921c
Recep Karamanlı <64321921+recepkaramanli@users.noreply.github.com>
2020-05-04 00:06:04 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
 const webdriver = require('selenium-webdriver')
 chrome = require('selenium-webdriver/chrome')
 var sleep = require('sleep-promise');
+let options = new chrome.Options();
+options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH);
+let serviceBuilder = new chrome.ServiceBuilder(process.env.CHROME_DRIVER_PATH);
+options.addArguments("--headless");
+options.addArguments("--disable-gpu");
+options.addArguments("--no-sandbox");
 
-    let options = new chrome.Options();
-    options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH);
-    let serviceBuilder = new chrome.ServiceBuilder(process.env.CHROME_DRIVER_PATH);
-
-    //Don't forget to add these for heroku
-    options.addArguments("--headless");
-    options.addArguments("--disable-gpu");
-    options.addArguments("--no-sandbox");
-
-async function sayfaac() {
- let driver = new webdriver.Builder()
+app.get('/', async function (req, res) {
+    
+    let driver = new webdriver.Builder()
     .forBrowser('chrome')
     .setChromeOptions(options)
     .setChromeService(serviceBuilder)
     .build();
-  //await driver.get('https://www.sahibinden.com/ilan/ikinci-el-ve-sifir-alisveris-bilgisayar-dizustu-notebook-dell-vostro-5590-i7-10510u-hatasiz-garantili-carsi-iletisimden-816835175/detay')
-  await driver.get('https://www.recepkaramanli.com/fiyat-takip');
-  await sleep(5000)
-  //await driver.getPageSource().then(function(res) { console.log(res)); })
-  await driver.getTitle().then(function (res) { console.log(res); });
-}
+      //await driver.get('https://www.sahibinden.com/ilan/ikinci-el-ve-sifir-alisveris-bilgisayar-dizustu-notebook-dell-vostro-5590-i7-10510u-hatasiz-garantili-carsi-iletisimden-816835175/detay')
+      await driver.get('https://www.recepkaramanli.com/fiyat-takip');
+      await sleep(5000)
+      //await driver.getPageSource().then(function(res) { console.log(res)); })
+      await driver.getTitle().then(function (res) { res.end(res); });
+  
+})
 
-sayfaac();
+var server = app.listen(PORT, function () {
+    console.log('sunucu calisiyor');
+})
