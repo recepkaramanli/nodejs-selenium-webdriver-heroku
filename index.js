5ce7dc484a10b41371c81216e5d39ebb45cba5ad
Recep Karamanlı <64321921+recepkaramanli@users.noreply.github.com>
2020-05-04 20:02:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ let options = new chrome.Options();
 options.setChromeBinaryPath(process.env.CHROME_BINARY_PATH);
 let serviceBuilder = new chrome.ServiceBuilder(process.env.CHROME_DRIVER_PATH);
 options.addArguments("--headless");
-//options.addArguments("--disable-gpu");
+options.addArguments("--disable-gpu");
 options.addArguments("--no-sandbox");
-//options.addArguments("--disable-web-security");
-//options.addArguments("--javascript-harmony");
+options.addArguments("user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36")
 
 app.get('/', async function (req, res) {
     
@@ -21,8 +20,8 @@ app.get('/', async function (req, res) {
     .setChromeOptions(options)
     .setChromeService(serviceBuilder)
     .build();
-      //await driver.get('https://www.hepsiburada.com/microsoft-xbox-one-s-kablosuz-oyun-kumandasi-beyaz-p-HBV000003SKRG?magaza=Nethouse')
-      await driver.get('https://www.recepkaramanli.com/fiyat-takip/islemtest.php');
+      await driver.get('https://www.hepsiburada.com/microsoft-xbox-one-s-kablosuz-oyun-kumandasi-beyaz-p-HBV000003SKRG?magaza=Nethouse')
+      //await driver.get('https://www.recepkaramanli.com/fiyat-takip/islemtest.php');
       //await sleep(5000)
       //await driver.getPageSource().then(function(res) { console.log(res)); })
       await driver.getTitle().then(function(title) { res.send(title); });
